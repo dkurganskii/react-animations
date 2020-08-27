0bdcf0f42fe9e5ca6dcc1165d56829d971e5ad8f
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-08-27 10:55:43 -0500

    prepare the demo project

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,11 +2,17 @@ import React from 'react';
 
 import './Modal.css';
 
-const modal = (props) => (
-    <div className="Modal">
-        <h1>A Modal</h1>
-        <button className="Button" onClick={props.closed}>Dismiss</button>
-    </div>
-);
+const modal = (props) => {
+	const cssClasses = [ 'Modal', props.show ? 'ModalOpen' : 'ModalClosed' ];
+
+	return (
+		<div className="Modal">
+			<h1>A Modal</h1>
+			<button className={cssClasses.join(' ')} onClick={props.closed}>
+				Dismiss
+			</button>
+		</div>
+	);
+};
 
 export default modal;
