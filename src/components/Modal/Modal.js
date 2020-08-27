a1ccd4bf85b012beb286c5a7c0df8e03c0174f31
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-08-27 10:59:40 -0500

    fix a modal bug

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -6,9 +6,9 @@ const modal = (props) => {
 	const cssClasses = [ 'Modal', props.show ? 'ModalOpen' : 'ModalClosed' ];
 
 	return (
-		<div className="Modal">
+		<div className={cssClasses.join(' ')}>
 			<h1>A Modal</h1>
-			<button className={cssClasses.join(' ')} onClick={props.closed}>
+			<button className="Button" onClick={props.closed}>
 				Dismiss
 			</button>
 		</div>
