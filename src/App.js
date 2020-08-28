04bd879f87b223bfbfe007aecf0f7bf9392a9009
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-08-27 19:06:16 -0500

    add transition events

--- src/App.js
+++ src/App.js
@@ -31,7 +31,18 @@ class App extends Component {
 					Toggle
 				</button>
 				<br />
-				<Transition in={this.state.showBlock} timeout={1000} mountOnEnter unmountOnExit>
+				<Transition
+					in={this.state.showBlock}
+					timeout={1000}
+					mountOnEnter
+					unmountOnExit
+					onEnter={() => console.log('onEnter')}
+					onEntering={() => console.log('onEntering')}
+					onEntered={() => console.log('onEntered')}
+					onExit={() => console.log('onExit')}
+					onExiting={() => console.log('onExiting')}
+					onExited={() => console.log('onExited')}
+				>
 					{(state) => (
 						<div
 							style={{
@@ -45,8 +56,8 @@ class App extends Component {
 						/>
 					)}
 				</Transition>
-				{this.state.modalIsOpen ? <Modal show={this.state.modalIsOpen} closed={this.closeModal} /> : null}
-				{this.state.modalIsOpen ? <Backdrop show={this.state.modalIsOpen} /> : null}
+				<Modal show={this.state.modalIsOpen} closed={this.closeModal} />
+				{this.state.modalIsOpen ? <Backdrop show /> : null}
 				<button className="Button" onClick={this.showModal}>
 					Open Modal
 				</button>
